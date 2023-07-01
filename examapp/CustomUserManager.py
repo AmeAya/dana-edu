419610c58320a77fd948983e8553e672c9894219
AmeAya <deobackstep@gmail.com>
2023-07-02 00:56:54 +0600

    CustomUser

---
+++ examapp/CustomUserManager.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import BaseUserManager
+from django.utils import timezone
+from datetime import datetime
+
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, iin, password, is_staff, is_superuser, **extra_fields):
+        if not iin:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            iin=iin,
+            birth_date=datetime.strptime(iin[:6], "%y%m%d").date(),
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            last_login=timezone.now(),
+            date_joined=timezone.now(),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, iin, password, **extra_fields):
+        return self._create_user(iin, password, False, False, **extra_fields)
+
+    def create_superuser(self, iin, password, **extra_fields):
+        user = self._create_user(iin, password, True, True, **extra_fields)
+        return user
--- examapp/models.py
+++ examapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.validators import MinLengthValidator
+from phonenumber_field.modelfields import PhoneNumberField
+from .CustomUserManager import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    iin = models.CharField(max_length=12, validators=[MinLengthValidator(12)], null=False, blank=False, unique=True)
+    surname = models.CharField(max_length=150, null=True, blank=True)
+    name = models.CharField(max_length=150, null=True, blank=True)
+    patronymic = models.CharField(max_length=150, null=True, blank=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    birth_date = models.DateField(null=False, blank=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'iin'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return str(self.surname) + ' ' + str(self.name) + ' ' + str(self.iin)
