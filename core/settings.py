8decec2868ce939470d5f5ef721f73e49c25347c
AmeAya <deobackstep@gmail.com>
2023-07-24 17:33:36 +0000

    server version 24 June 2023

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h$(t60bhltgdgfe6!4slti%!w%lh$#2^(i2l))c%jfdft$1(g!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['danaedu.pythonanywhere.com']
 
 
 # Application definition
